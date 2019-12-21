e0f3ccf9b6f18b5afd873300c05e33845906f0dc
krewaz234 <53952343+krewaz234@users.noreply.github.com>
2019-12-21 21:36:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "TXLib.h"
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 #define WIND_X_SIZE 800
 #define WIND_Y_SIZE 800
 #define MOUSE_LEFT_CLICK 1
 #define MOUSE_RIGHT_CLICK 2
+#define DEFAULT_START_SCORE 10
 
 enum game_mode
 {
@@ -20,8 +22,27 @@ enum main_menu_button
 {
     PLAY,
     SETTINGS,
-    QUIT
+    QUIT,
 };
+
+enum before_start_button
+{
+    DECREASE_SCORE,
+    INCREASE_SCORE,
+};
+
+inline void number_to_numerals(int number, std::vector<int> &numerals)
+{
+    std::vector<int> ans;
+    while (number >= 10)
+    {
+        ans.push_back(number % 10);
+        number /= 10;
+    }
+    ans.push_back(number);
+    std::reverse(ans.begin(), ans.end());
+    numerals = ans;
+}
 
 inline void draw_main_menu(HDC title, HDC start_button, HDC settings_button, HDC quit_button)
 {
@@ -47,15 +68,32 @@ inline void draw_main_menu(HDC title, HDC start_button, HDC settings_button, HDC
 
 }
 
-inline void draw_before_game(HDC score_to_victory)
+inline void draw_before_game(HDC score_to_victory, HDC dec_stv, HDC inc_stv, HDC *numerals, int stv, std::vector<RECT> &buttons)
 {
     int
         stv_margin_left = (WIND_X_SIZE - 523) / 2,
         stv_margin_top  = 100;
     txBitBlt(txDC(), stv_margin_left, stv_margin_top, 523, 59, score_to_victory);
+
+    std::vector<int> score_numerals;
+    number_to_numerals(stv, score_numerals);
+    int number_width = 65 * (score_numerals.size() + 1);
+    int score_left_margin = (WIND_X_SIZE - number_width - 65) / 2;
+    int number_margin_stv = 80;
+    //refreshing button sizes here because of the temporary size of the number
+    std::vector<RECT> temp;
+    txBitBlt(txDC(), score_left_margin, stv_margin_top + number_margin_stv + 15, 50, 50, dec_stv);
+    temp.push_back({score_left_margin, stv_margin_top + number_margin_stv + 15, score_left_margin + 50, stv_margin_top + number_margin_stv + 50});
+    for (size_t i = 0; i < score_numerals.size(); ++i)
+    {
+        txBitBlt(txDC(), score_left_margin + (i + 1)*65, stv_margin_top + number_margin_stv, 65, 80, numerals[score_numerals[i]]);
+    }
+    txBitBlt(txDC(), score_left_margin + number_width + 10, stv_margin_top + number_margin_stv + 15, 50, 50, inc_stv);
+    temp.push_back({score_left_margin + number_width + 10, stv_margin_top + number_margin_stv + 15, score_left_margin + number_width + 50, stv_margin_top + number_margin_stv + 50});
+    buttons = temp;
 }
 
-inline void fill_buttons_coords(std::vector<RECT> &buttons, game_mode current_mode)
+inline void fill_buttons_coords(std::vector<RECT> &buttons, game_mode current_mode, std::vector<int> left_button_coord = {}, std::vector<int> right_button_coord = {})
 {
     std::vector<RECT> temp;
     if (current_mode == MAIN_MENU)
@@ -77,11 +115,6 @@ inline void fill_buttons_coords(std::vector<RECT> &buttons, game_mode current_mo
     buttons = temp;
 }
 
-inline void hide_HDC(HDC button)
-{
-    txBitBlt(txDC(), 100, 100, 1, 1, button);
-}
-
 inline void free_HDC_mem(HDC pic, ...)
 {
     HDC *it = &pic;
@@ -92,21 +125,9 @@ inline void free_HDC_mem(HDC pic, ...)
     }
 }
 
-std::vector<int> number_to_numerals(int number)
-{
-    std::vector<int> ans;
-    while (number <= 10)
-    {
-        ans.push_back(number % 10);
-        number /= 10;
-    }
-    ans.push_back(number);
-    std::reverse(ans.begin(), ans.end());
-    return ans;
-}
-
 int main()
 {
+
     txCreateWindow(WIND_X_SIZE, WIND_Y_SIZE);
     txSetFillColor(TX_BLACK);
 
@@ -166,8 +187,22 @@ int main()
         return 1;
     }
 
-    const HDC numerals[] = {
+    HDC inc_stv = txLoadImage("Resources\\inc_stv.bmp");
+    if (!inc_stv)
+    {
+        MessageBox(GetActiveWindow(), "Failed to load inc_stv.bmp", "Fatal error", MB_ICONERROR);
+        return 1;
+    }
 
+    HDC dec_stv = txLoadImage("Resources\\dec_stv.bmp");
+    if (!dec_stv)
+    {
+        MessageBox(GetActiveWindow(), "Failed to load dec_stv.bmp", "Fatal error", MB_ICONERROR);
+        return 1;
+    }
+
+    HDC numerals[] =
+    {
         txLoadImage("Resources\\0.bmp"),
         txLoadImage("Resources\\1.bmp"),
         txLoadImage("Resources\\2.bmp"),
@@ -178,28 +213,29 @@ int main()
         txLoadImage("Resources\\7.bmp"),
         txLoadImage("Resources\\8.bmp"),
         txLoadImage("Resources\\9.bmp"),
-
     };
 
     for (int i = 0; i < 10; ++i)
+    {
         if (!numerals[i])
         {
-            std::string error_cause = "Failed to load ";
-            error_cause += std::to_string(i);
-            error_cause += ".bmp";
+            std::string error_cause = "Failed to load " + std::to_string(i) + ".bmp";
             MessageBox(GetActiveWindow(), error_cause.c_str(), "Fatal error", MB_ICONERROR);
-            exit(1);
+            return 1;
         }
+    }
+
 
     game_mode current_mode = MAIN_MENU;
     std::vector<RECT> screen_buttons;
+    int victory_score = DEFAULT_START_SCORE;
     while (!GetAsyncKeyState(VK_ESCAPE))
     {
         int mouse_button = txMouseButtons();
-        fill_buttons_coords(screen_buttons, current_mode);
         if (current_mode == MAIN_MENU)
         {
             //need to nide all other buttons
+            fill_buttons_coords(screen_buttons, current_mode);
             draw_main_menu(title, start_button, settings_button, quit_button);
             if (txMouseButtons() == MOUSE_LEFT_CLICK)
             {
@@ -227,14 +263,30 @@ int main()
 
         if (current_mode == BEFORE_START)
         {
-            draw_before_game(score_to_victory);
+            draw_before_game(score_to_victory, dec_stv, inc_stv, numerals, victory_score, screen_buttons);
+            if (txMouseButtons() == MOUSE_LEFT_CLICK)
+            {
+                POINT cursor_pos = txMousePos();
+                if (In(cursor_pos, screen_buttons[DECREASE_SCORE]) && victory_score > 1)
+                {
+                    --victory_score;
+                    txSleep(100);
+                }
 
+                if (In(cursor_pos, screen_buttons[INCREASE_SCORE]) && victory_score < 1000)
+                {
+                    ++victory_score;
+                    txSleep(100);
+                }
+
+
+            }
         }
         txSleep(20);
         txClear();
     }
 
-    free_HDC_mem(title, start_button, settings_button, quit_button, left_arrow, right_arrow, score_to_victory);
+    free_HDC_mem(title, start_button, settings_button, quit_button, left_arrow, right_arrow, score_to_victory, inc_stv, dec_stv);
     for (int i = 0; i < 10; ++i)
         free_HDC_mem(numerals[i]);
     txClear();
